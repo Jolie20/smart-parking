a49de46a2affe76ba10f6ca65a45daf44fd3bcb0
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-18 16:11:03 +0200

    updates

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,7 +6,7 @@ const admin = require('../controllers/adminController');
 
 router.post('/login',auth.AdminLogin);//completed
 router.post('/create',admin.Adminseed);//completed
-router.use(authenticate, requireRole('ADMIN'));
+router.use(authenticate, requireRole('ADMIN'));//completed
 router.get('/stats', admin.getSystemStats);
 router.get('/users', admin.listUsers);
 router.post('/assign-manager', admin.assignManagerToLot);
