5ddcc2bf9d261574ae7f8fa14665b7730ae4517c
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-18 23:08:58 +0200

    updates

--- server/controllers/parkingSpotController.js
+++ server/controllers/parkingSpotController.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 exports.createSpot = async (req, res) => {
   try {
     const { spotNumber, isAvailable, isReserved, vehicleId,lotname } = req.body;
-    const parkingLot = await prisma.parkingLot.findUnique({ where: { name: lotname } });
+    const parkingLot = await prisma.ParkingLot.findUnique({ where: { name: lotname } });
     if (!lot) return res.status(400).json({ error: 'Invalid lotId' }); 
     const spot = await prisma.ParkingSpot.create({ data: { spotNumber, isAvailable, isReserved, vehicleId, lotId:parkingLot.id } });
     res.status(201).json(spot);
