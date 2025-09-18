00b2a9b95dba53ae5cf61a75faa66031cac8cad4
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-19 00:48:34 +0200

    updates

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
-const prisma = require('../generated/prisma');
+const {PrismaClient} = require('../generated/prisma');
+const prisma = new PrismaClient();
 
 exports.createBooking = async (req, res) => {
   try {
