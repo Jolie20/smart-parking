79983d149136ca6b0950b437e1f528bc540cd043
Jolie20 <jolieamanda350@gmail.com>
2025-09-19 10:23:19 +0200

    updates

--- project/server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../controllers/authController');
-
-router.post('/login', auth.login);
-
-module.exports = router;
-
-
--- project/server/routes/cardDataRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const cardDataController = require('../controllers/cardDataController');
-
-router.post('/', cardDataController.createCard);
-router.get('/', cardDataController.getCards);
-
-module.exports = router;
