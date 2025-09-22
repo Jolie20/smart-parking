c34dc890cab8d67cb22eb1d7ad5d47c9080b1c00
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-22 17:31:05 +0200

    updates

--- server/routes/manager.js
+++ server/routes/manager.js
@@ -4,7 +4,6 @@ const { authenticate, requireRole, ManagerLogin } = require('../middleware/auth'
 const manager = require('../controllers/managerController');
 
 router.post('/login', ManagerLogin);
-router.use(authenticate, requireRole('manager', 'admin'));
 router.get('/lots', manager.getManagedLots);
 router.get('/sessions', manager.getManagedSessions);
 
