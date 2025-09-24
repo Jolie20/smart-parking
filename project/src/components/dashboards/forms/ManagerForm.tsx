4f936bbb9b2f222b0b66ac931dd9183638fc5338
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-24 17:43:13 +0200

    update

--- project/src/components/dashboards/forms/ManagerForm.tsx
+++ project/src/components/dashboards/forms/ManagerForm.tsx
@@ -75,7 +75,7 @@ const ManagerForm: React.FC<ManagerFormProps> = ({
     }finally {
       setIsloading(false);
     }
-    onSubmit({ Data: formData });
+    onSubmit({ fromData : formData });
     
   };
 
