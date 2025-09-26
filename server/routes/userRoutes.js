44d3bfe9f93c93d321dcecc459db8ea5c94a17ad
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2025-09-26 17:28:47 +0200

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,8 +9,8 @@ router.post('/', userController.createUser);
 router.post('/v1/login', auth.UserLogin);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
+router.put('/:id', userController.updateUser);  
 router.delete('/:id', userController.deleteUser);
-router.get('/myvehicle',authenticate,uservehicles);
+router.get('/myvehicle',authenticate, uservehicles);
 
 module.exports = router;
